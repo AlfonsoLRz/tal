3f0850960bedf81405f5d946660486703e7c2a77
Alfonso López Ruiz <allopezr@ujaen.es>
2025-09-04 16:11:49 +0200

    Update __init__.py

--- tal/reconstruct/fk/__init__.py
+++ tal/reconstruct/fk/__init__.py
@@ -53,14 +53,6 @@ def solve(data: NLOSCaptureData) -> NLOSCaptureData.SingleReconstructionType:
         # print(N, M, N * range / (M * width * 4))
         #
 
-        # z, y, x = cp.mgrid[-M:M, -N:N, -N:N].astype(float_dtype)
-        # z, y, x = z / M, y / N, x / N  # Normalize to [-1, 1] range
-
-        # epsilon = 1e-8
-        # sqrt_term = cp.sqrt((N*range/(M*width*4))**2 * (x**2 + y**2) + z**2)
-        # grid_z = cp.tile(cp.linspace(0, 1, M, dtype=float_dtype)[:, cp.newaxis, cp.newaxis], (1, N, N))
-        # data = cp.sqrt(data*grid_z**2).astype(complex_dtype)  
-
         # FFT
         t_data = cp.zeros((2*N, 2*N, 2*M), dtype=complex_dtype)  
         t_data[:N, :N, :M] = data
@@ -74,6 +66,7 @@ def solve(data: NLOSCaptureData) -> NLOSCaptureData.SingleReconstructionType:
             (M + threads_per_block[2] - 1) // threads_per_block[2],
         )
 
+        print(N, N, M)
         out_data = cp.zeros((N, N, M), dtype=complex_dtype)
         fk_numba[blocks_per_grid, threads_per_block](t_data, out_data, N * range / (M * width * 4))
 
@@ -100,54 +93,54 @@ def fk_numba(
     d_H_1,                 # Output data        
     stolt_const
 ):
-    # res_x, res_y, res_t = d_H_0.shape
-    # epsilon = 1e-8
+    resolution = d_H_0.shape
+    epsilon = 1e-8
 
-    v = cuda.blockIdx.x * cuda.blockDim.x + cuda.threadIdx.x
-    l = cuda.blockIdx.y * cuda.blockDim.y + cuda.threadIdx.y
-    s = cuda.blockIdx.z * cuda.blockDim.z + cuda.threadIdx.z
+    x = cuda.blockIdx.x * cuda.blockDim.x + cuda.threadIdx.x
+    y = cuda.blockIdx.y * cuda.blockDim.y + cuda.threadIdx.y
+    z = cuda.blockIdx.z * cuda.blockDim.z + cuda.threadIdx.z
     
-    # if t >= res_t or x >= res_x or y >= res_y:
-    #     return
+    if z >= resolution[2] or x >= resolution[0] or y >= resolution[1]:
+        return
     
-    # fx = 2.0 * float(x) / float(resolution[0]) - 1.0
-    # fy = 2.0 * float(y) / float(resolution[1]) - 1.0
-    # fz = 2.0 * float(t) / float(resolution[2]) - 1.0
+    fx = 2.0 * float(x) / float(resolution[0]) - 1.0
+    fy = 2.0 * float(y) / float(resolution[1]) - 1.0
+    fz = 2.0 * float(z) / float(resolution[2]) - 1.0
 
-    # sqrt_term = math.sqrt(stolt_const * (fx*fx + fy*fy + fz*fz))
+    sqrt_term = math.sqrt(stolt_const * (fx*fx + fy*fy) + fz*fz)
 
-    # ix = (fx + 1.0) * 0.5 * resolution[0]
-    # iy = (fy + 1.0) * 0.5 * resolution[1]
-    # iz = (fz + 1.0) * 0.5 * resolution[2]
+    ix = (fx + 1.0) * 0.5 * resolution[0]
+    iy = (fy + 1.0) * 0.5 * resolution[1]
+    iz = (sqrt_term + 1.0) * 0.5 * resolution[2]
 
-    # x0 = int(math.floor(ix))
-    # y0 = int(math.floor(iy))
-    # z0 = int(math.floor(iz))
-    # x1, y1, z1 = x0 + 1, y0 + 1, z0 + 1
-    # dx, dy, dz = ix - x0, iy - y0, iz - z0
+    x0 = int(math.floor(ix))
+    y0 = int(math.floor(iy))
+    z0 = int(math.floor(iz))
+    x1, y1, z1 = x0 + 1, y0 + 1, z0 + 1
+    dx, dy, dz = ix - x0, iy - y0, iz - z0
 
-    # x0 = max(0, min(x0, resolution[0] - 1))
-    # y0 = max(0, min(y0, resolution[1] - 1))
-    # z0 = max(0, min(z0, resolution[2] - 1))
-    # x1 = max(0, min(x1, resolution[0] - 1))
-    # y1 = max(0, min(y1, resolution[1] - 1))
-    # z1 = max(0, min(z1, resolution[2] - 1))
+    x0 = max(0, min(x0, resolution[0] - 1))
+    y0 = max(0, min(y0, resolution[1] - 1))
+    z0 = max(0, min(z0, resolution[2] - 1))
+    x1 = max(0, min(x1, resolution[0] - 1))
+    y1 = max(0, min(y1, resolution[1] - 1))
+    z1 = max(0, min(z1, resolution[2] - 1))
 
-    # c000 = d_H_0[x0, y0, z0]
-    # c001 = d_H_0[x1, y0, z0]
-    # c010 = d_H_0[x0, y1, z0]
-    # c011 = d_H_0[x1, y1, z0]
-    # c100 = d_H_0[x0, y0, z1]
-    # c101 = d_H_0[x1, y0, z1]
-    # c110 = d_H_0[x0, y1, z1]
-    # c111 = d_H_0[x1, y1, z1]
-    # c00 = c000 * (1 - dx) + c001 * dx
-    # c01 = c010 * (1 - dx) + c011 * dx
-    # c10 = c100 * (1 - dx) + c101 * dx
-    # c11 = c110 * (1 - dx) + c111 * dx
-    # c0 = c00 * (1 - dy) + c01 * dy
-    # c1 = c10 * (1 - dy) + c11 * dy
-    # c = c0 * (1 - dz) + c1 * dz
-    # d = math.abs(fz) / (sqrt_term + epsilon)
+    c000 = d_H_0[x0, y0, z0]
+    c001 = d_H_0[x1, y0, z0]
+    c010 = d_H_0[x0, y1, z0]
+    c011 = d_H_0[x1, y1, z0]
+    c100 = d_H_0[x0, y0, z1]
+    c101 = d_H_0[x1, y0, z1]
+    c110 = d_H_0[x0, y1, z1]
+    c111 = d_H_0[x1, y1, z1]
+    c00 = c000 * (1 - dx) + c001 * dx
+    c01 = c010 * (1 - dx) + c011 * dx
+    c10 = c100 * (1 - dx) + c101 * dx
+    c11 = c110 * (1 - dx) + c111 * dx
+    c0 = c00 * (1 - dy) + c01 * dy
+    c1 = c10 * (1 - dy) + c11 * dy
+    c = c0 * (1 - dz) + c1 * dz
+    d = math.fabs(fz) / (sqrt_term + epsilon)
 
-    # d_H_1[x, y, t] = c * d
+    d_H_1[x, y, z] = 1.0
